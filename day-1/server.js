4a263283c98a765d314b88428b8f514111beadfd
Punit Kumar <tagpunitkumar@gmail.com>
2023-08-30 16:58:51 +0530

    half-done

--- day-1/server.js
+++ day-1/server.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());//Lets us use json in the body of the request.
 
 const posts = [
     {
@@ -12,9 +17,30 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
+
+app.post('/login', (req, res) => {
+    //Authentication
+    const username = req.body.username;
+    const user = { name: username }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
+    res.json({ accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return req.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+    next()
+    })
+}
 
 
 app.listen(3000)
